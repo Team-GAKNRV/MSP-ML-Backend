246f7c7b51e6b44d531c527663dfe4e1d8e28889
topgggggg <156314616+topgggggg@users.noreply.github.com>
2024-05-23 14:06:54 +0200

    MSP-47 add constants

---
+++ core/scripts/constants.py
@@ -0,0 +1,25 @@
+CLASSIFIED_LABELS_ARRAY = {
+    "articleType": "",
+    "baseColour": "",
+    "season": "",
+    "usage": ""
+}
+
+PATH_LABELS = "core/labels"
+PATH_MODELS = "core/models"
+PATH_TEMP_FILES = "temp"
+
+MODEL_ARTICLE_TYPE = f"{PATH_MODELS}/article_type.pt"
+MODEL_BASE_COLOUR = f"{PATH_MODELS}/base_colour.pt"
+MODEL_SEASON = f"{PATH_MODELS}/season.pt"
+MODEL_USAGE = f"{PATH_MODELS}/usage.pt"
+
+LAYERS_ARTICLE_TYPE = 33
+LAYERS_BASE_COLOUR = 46
+LAYERS_SEASON = 4
+LAYERS_USAGE = 8
+
+TAG_NAME_ARTICLE_TYPE = "article"
+TAG_NAME_BASE_COLOUR = "base"
+TAG_NAME_SEASON = "season"
+TAG_NAME_USAGE = "usage"
