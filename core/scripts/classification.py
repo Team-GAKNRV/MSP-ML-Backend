87fa21e5fcca8a919c8b32b19e977b82cb02101c
topgggggg <156314616+topgggggg@users.noreply.github.com>
2024-05-24 23:38:55 +0200

    MSP-47 add classification script

---
+++ core/scripts/classification.py
@@ -0,0 +1,43 @@
+import torch
+import cv2
+import torchvision.transforms as transforms
+import numpy as np
+import joblib
+
+from core.scripts.models import SingleHeadResNet50
+from src.loss_functions import single_loss_fn, loss_fn
+
+
+def classify_single_label(model_path: str, layers_count: int, tag_name: str, input_image_path: str) -> str:
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = SingleHeadResNet50(pretrained=False, requires_grad=False, layers=layers_count)
+    checkpoint = torch.load(model_path, map_location=device)
+    
+    model.load_state_dict(checkpoint['model_state_dict'])
+    model.to(device)
+    model.eval()
+
+    transform = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+
+    image = cv2.imread(input_image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = transform(image)
+    image = image.unsqueeze(0).to(device)
+    
+    with torch.no_grad():
+        output = model(image)
+    
+    out_label = np.argmax(output.cpu().numpy())
+    num_list_data = joblib.load(f"src/num_list_{tag_name}.pkl")
+
+    data_keys = list(num_list_data.keys())
+    data_values = list(num_list_data.values())
+    
+    final_label = data_keys[data_values.index(out_label)]
+    
+    return final_label
