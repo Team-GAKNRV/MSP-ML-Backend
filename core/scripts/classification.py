bf5f00133e76f94c9d5c727418589bf4248da0ff
topgggggg <156314616+topgggggg@users.noreply.github.com>
2024-05-25 01:01:48 +0200

    MSP-47 move loss_functions to root folder with explanation
    This had to be done due to https://github.com/pytorch/pytorch/issues/18325

--- core/scripts/classification.py
+++ core/scripts/classification.py
@@ -1,11 +1,12 @@
-import torch
 import cv2
-import torchvision.transforms as transforms
-import numpy as np
 import joblib
+import numpy as np
+import torch
+import torchvision.transforms as transforms
 
+# loss_fn has to be imported due to -> https://github.com/pytorch/pytorch/issues/18325
+from loss_functions import loss_fn
 from core.scripts.constants import PATH_LABELS
-from core.scripts.loss_functions import loss_fn
 from core.scripts.models import SingleHeadResNet50
 
 
@@ -13,7 +14,7 @@ def classify_single_label(model_path: str, layers_count: int, tag_name: str, inp
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = SingleHeadResNet50(pretrained=False, requires_grad=False, layers=layers_count)
     checkpoint = torch.load(model_path, map_location=device)
-    
+
     model.load_state_dict(checkpoint['model_state_dict'])
     model.to(device)
     model.eval()
@@ -29,16 +30,16 @@ def classify_single_label(model_path: str, layers_count: int, tag_name: str, inp
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image = transform(image)
     image = image.unsqueeze(0).to(device)
-    
+
     with torch.no_grad():
         output = model(image)
-    
+
     out_label = np.argmax(output.cpu().numpy())
     num_list_data = joblib.load(f"{PATH_LABELS}/{tag_name}.pkl")
 
     data_keys = list(num_list_data.keys())
     data_values = list(num_list_data.values())
-    
+
     final_label = data_keys[data_values.index(out_label)]
-    
+
     return final_label
