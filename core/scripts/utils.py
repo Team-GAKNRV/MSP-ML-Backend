3b09eccc16b61ca8f3ec30454928a6b19047d637
topgggggg <156314616+topgggggg@users.noreply.github.com>
2024-05-30 23:01:34 +0200

    MSP-58 move cv2 import to inline import

--- core/scripts/utils.py
+++ core/scripts/utils.py
@@ -1,6 +1,6 @@
-import cv2
-
 def resize_image_to_fixed_height(image, fixed_height=900):
+    import cv2
+    
     original_height, original_width = image.shape[:2]
     ratio = fixed_height / original_height
     new_width = int(original_width * ratio)
