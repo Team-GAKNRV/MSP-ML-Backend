29862d3ec974cf0c3aa3f17bc161f2ddfd421e44
topgggggg <156314616+topgggggg@users.noreply.github.com>
2024-06-01 22:55:47 +0200

    MSP-62 add cors configuration for post requests from the frontend

--- main.py
+++ main.py
@@ -2,12 +2,24 @@ import os
 
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 from api.v1.api import api_router
 
 load_dotenv()
 
 app = FastAPI()
+
+origins = [f"http://localhost:{os.getenv("FRONTEND_PORT")}"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["POST"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router, prefix="/api")
 
